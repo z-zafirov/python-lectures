91786f92cee205e0a31db6ed26fa31e4627a1a6b
Zdravko Zafirov <zdravko.zafirov@gmail.com>
2019-10-20 23:48:38 +0300

    Adding almost done solution for module 4 - task 2

---
+++ module04/02-Bank-Account/solution.py
@@ -0,0 +1,48 @@
+class BankAccount():
+
+    def __init__(self, name, currency, balance=0):
+        self.name = name
+        self.currency = currency
+        try:
+            if balance >= 0:
+                self.balance = balance
+            elif balance < 0:
+                raise ValueError('Negative value provided for balance.')
+        except ValueError  as exc:
+            self.balance = f'Exception: {exc}'
+    
+    def __str__(self):
+        return f'Bank account for {self.name} with balance of {self.balance} {self.currency}'
+
+    def __int__(self):
+        return int(self.balance)
+
+    def deposit(self, amount):
+        try:
+            if amount < 0:
+                raise ValueError('Negative value provided for deposit amount.')
+            elif amount > 0:
+                self.balance = self.balance + amount
+        except ValueError  as exc:
+            self.balance = f'Exception: {exc}'
+
+    def withdraw(self, amount):
+        if amount < 0:
+            return False
+        elif amount > 0:
+            self.balance = self.balance - amount
+            return True
+
+#'''
+o1 = BankAccount('Jack', 'BGN', 30)
+print(str(o1))
+o1.deposit(10)
+print(str(o1))
+t1 = o1.withdraw(20)
+print(str(o1))
+print(t1)
+t2 = o1.withdraw(-10)
+print(str(o1))
+print(t2)
+print(int(o1))
+#'''
