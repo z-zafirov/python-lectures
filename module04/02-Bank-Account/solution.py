a556f3fb0b6fd36574a52c5b76ad3305eb61d914
Zdravko Zafirov <zdravko.zafirov@gmail.com>
2019-10-23 23:45:24 +0300

    Finished module 4 - task 2

--- module04/02-Bank-Account/solution.py
+++ module04/02-Bank-Account/solution.py
@@ -22,39 +22,68 @@ class BankAccount():
     def __int__(self):
         return int(self.balance)
 
-    def deposit(self, amount):
+    def deposit(self, d_amount):
         try:
-            if amount <= 0:
+            if d_amount <= 0:
                 raise ValueError('Negative or null value provided for deposit amount.')
-            elif amount > 0:
-                self.balance = self.balance + amount
+            elif d_amount > 0:
+                self.balance = self.balance + d_amount
                 timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-                self.history_list.append(f'{timestamp} | {amount} {self.currency} were deposited on {self.name}\'s account.')
+                self.history_list.append(f'{timestamp} | {d_amount} {self.currency} were deposited on {self.name}\'s account.')
         except ValueError  as exc:
             self.balance = f'Exception: {exc}'
 
-    def withdraw(self, amount):
-        if amount <= 0:
+    def withdraw(self, w_amount):
+        if w_amount <= 0:
             return False
-        elif amount > 0:
-            self.balance = self.balance - amount
+        elif w_amount > 0:
+            self.balance = self.balance - w_amount
             timestamp = timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            self.history_list.append(f'{timestamp} | {amount} {self.currency} were withdrawn from {self.name}\'s account.')
+            self.history_list.append(f'{timestamp} | {w_amount} {self.currency} were withdrawn from {self.name}\'s account.')
             return True
+
+    def account_currency(self):
+        return self.currency
+
+    def transfer_to(self, account, t_amount):
+        account_currency = account.account_currency()
+        if self.currency == account_currency:
+            self.balance = int(self.balance - t_amount)
+            account.balance = int(account.balance + t_amount)
+            print('Transfer successful.')
+        else:
+            print('Different currencies, transfer not successful.')
+            
+
+'''
+`transfer_to(account, amount)` - transfers `amount` to `account` if they both have the same currencies! Returns `True` if successful.
+'''
 
 #'''
 o1 = BankAccount('Jack Doe', 'BGN', 30)
+o2 = BankAccount('Jack Richar', 'USD', 50)
+o3 = BankAccount('Jane Smith', 'BGN', 30)
+
 print(str(o1))
 o1.deposit(10)
 print(str(o1))
+
 t1 = o1.withdraw(20)
 print(f'withdraw status for 20: {t1}')
 print(str(o1))
 t2 = o1.withdraw(-10)
 print(f'withdraw status for -10: {t2}')
-print('Testing int() and str() below:')
+
 print(str(o1))
 print(int(o1))
-print('Testing the history list below:')
+
 print(o1.history_list)
-#'''
+
+print(str(o1))
+print(str(o2))
+print(str(o3))
+o1.transfer_to(o3, 10)
+o1.transfer_to(o2, 10)
+print(str(o1))
+print(str(o2))
+print(str(o3))
