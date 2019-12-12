104011311f28c0287e6827637c63414a2206a437
Zdravko Zafirov <zdravko.zafirov@gmail.com>
2019-12-12 21:46:36 +0200

    Adding test_calculator.py to the repo

--- module05/test_calculator.py
+++ module05/test_calculator.py
@@ -2,13 +2,34 @@ import pytest
 import unittest
 import calculator
 
-class MyTestClass(unittest.TestCase):
+'''
+class Prequels(unittest.TestCase):
+
+    def setUp(self):
+        self.test_calc = calculator
+        # print("    setUp executes before every test method!")
+
+    def tearDown(self):
+        print("    tearDown executes after every test method!")
+'''
+#class CalculatorTestCase(Prequels):
+class CalculatorTestCase(unittest.TestCase):
     
     def setUp(self):
-        self.test_calc = calculator()
+        self.test_calc = calculator.Calculator()
 
-    def test_add_true(self):
-        self.x = 3
-        self.y = 4
-        self.calculator_add = self.test_calc.add(self.x, self.y)
-        assert (self.x + self.y) == self.calculator_add, 'Test failed.'
+    def test_add(self):
+        self.add_result = self.test_calc.add(4, 2)
+        assert self.add_result == 6, 'Test failed.'
+
+    def test_subtract(self):
+        self.add_result = self.test_calc.subtract(4, 2)
+        assert self.add_result == 2, 'Test failed.'
+
+    def test_multiply(self):
+        self.add_result = self.test_calc.multiply(4, 2)
+        assert self.add_result == 8, 'Test failed.'
+
+    def test_divide(self):
+        self.add_result = self.test_calc.divide(4, 2)
+        assert self.add_result == 2, 'Test failed.'
