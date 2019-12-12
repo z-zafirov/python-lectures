380705d2a77a8760538ba9b4722e4f8e46d959e5
Zdravko Zafirov <zdravko.zafirov@gmail.com>
2019-12-12 22:01:08 +0200

    Renamed all add_results to result

--- module05/test_calculator.py
+++ module05/test_calculator.py
@@ -2,34 +2,23 @@ import pytest
 import unittest
 import calculator
 
-'''
-class Prequels(unittest.TestCase):
-
-    def setUp(self):
-        self.test_calc = calculator
-        # print("    setUp executes before every test method!")
-
-    def tearDown(self):
-        print("    tearDown executes after every test method!")
-'''
-#class CalculatorTestCase(Prequels):
 class CalculatorTestCase(unittest.TestCase):
     
     def setUp(self):
         self.test_calc = calculator.Calculator()
 
     def test_add(self):
-        self.add_result = self.test_calc.add(4, 2)
-        assert self.add_result == 6, 'Test failed.'
+        self.result = self.test_calc.add(4, 2)
+        assert self.result == 6, 'Test failed.'
 
     def test_subtract(self):
-        self.add_result = self.test_calc.subtract(4, 2)
-        assert self.add_result == 2, 'Test failed.'
+        self.result = self.test_calc.subtract(4, 2)
+        assert self.result == 2, 'Test failed.'
 
     def test_multiply(self):
-        self.add_result = self.test_calc.multiply(4, 2)
-        assert self.add_result == 8, 'Test failed.'
+        self.result = self.test_calc.multiply(4, 2)
+        assert self.result == 8, 'Test failed.'
 
     def test_divide(self):
-        self.add_result = self.test_calc.divide(4, 2)
-        assert self.add_result == 2, 'Test failed.'
+        self.result = self.test_calc.divide(4, 2)
+        assert self.result == 2, 'Test failed.'
