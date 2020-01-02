786e77ab6ef66ff8b99790aeaa21596426763604
Zdravko Zafirov <zdravko.zafirov@gmail.com>
2020-01-02 05:22:24 +0200

    Adding the Vote project dir

---
+++ django-tutorial/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/zdravko/Documents/Python/python-lectures/django-tutorial/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
